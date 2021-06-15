03fcd622f5e18e539b097b083190b07b947580b2
Sirish Pilla <sirishpilla@gmail.com>
2021-06-15 00:16:32 -0500

    Profile Commit

--- config/api/profile.js
+++ config/api/profile.js
@@ -1,10 +1,27 @@
 const express=require('express');
 
 const router=express.Router();
-
-//@route    GET api/profile
-//@desc     Test route 
-//@access   Public ---- If we need a token to access a route it is called Private
-router.get('/',(req,res)=> res.send('Profile ROute'));
-
+const auth= require('../../middleware/auth');
+const Profile=require('../../models/Profile');
+const User=require('../../models/User');
+//@route    GET api/profile/me
+//@desc     Get current users profile
+//@access   Private ---- If we need a token to access a route it is called Private
+router.get('/me',auth,async (req,res)=>{
+  
+    
+    try{
+        const profile = await Profile.findOne({ user:req.user.id }).populate(
+            'user',
+            ['name','avatar']
+        );
+        if(!profile){
+            return res.status(400).json[{msg:"There is no profile for this user"}]
+        }
+    res.json(profile);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+});
 module.exports = router;
